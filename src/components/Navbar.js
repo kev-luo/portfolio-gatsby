ddb855a4d9ea8080e284f32bbf535d369358058d
Kevin Luo <kvn.luo@gmail.com>
2020-12-06 14:27:25 -0500

    add link constants

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header"></div>
+        
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/links.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const data = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about/",
+  },
+  {
+    id: 1,
+    text: "contact",
+    url: "/contact/",
+  },
+  {
+    id: 1,
+    text: "portfolio",
+    url: "/portfolio/",
+  },
+]
+
+export default ({ style }) => {
+  return (
+    <ul className={`${style ? style : ""}`}>
+      {data.map(link => (
+        <li key={link.id}>
+          <Link to={link.url}>{link.text}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
