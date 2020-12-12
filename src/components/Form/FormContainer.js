b4dbb6a34f99a6dc925cf47543cd8d163d98b910
Kevin Luo <kvn.luo@gmail.com>
2020-12-11 22:51:32 -0500

    neumorphism form styling

--- src/components/Form/FormContainer.js
+++ src/components/Form/FormContainer.js
@@ -7,8 +7,65 @@ const Container = styled.div`
   align-items: center;
 `
 
-const FormContainer = styled.div`
+const StyledForm = styled.form`
   display: flex;
+  flex-direction: column;
+  width: 25%;
+  padding: 2rem;
+  margin-top: 2rem;
+  box-shadow: 11px 11px 11px 0px rgba(0, 0, 0, 0.1),
+    -11px -11px 11px 0px rgba(77, 77, 77, 0.1);
+  border: 1px solid var(--clr-bg-1);
+  border-radius: 25px;
+  background-color: var(--clr-bg-1);
+`
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+  font-size: 1.25rem;
+`
+
+const Input = styled.input`
+  margin-top: 0.25rem;
+  border: 2px solid transparent;
+  border-radius: 25px;
+  outline: none;
+  padding: 0.25rem;
+  &:focus {
+    border-color: var(--clr-primary-5);
+  }
+`
+
+const TextArea = styled.textarea`
+  margin-top: 0.25rem;
+  border: 2px solid transparent;
+  border-radius: 25px;
+  outline: none;
+  padding: 0.25rem;
+  &:focus {
+    border-color: var(--clr-primary-5);
+  }
+`
+
+const Button = styled.button`
+  cursor: pointer;
+  width: 8rem;
+  font-size: 1rem;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  padding: 1rem 2rem;
+  margin-top: 0.5rem;
+  background: var(--clr-bg-1);
+  border: 2px solid var(--clr-bg-1);
+  color: var(--clr-primary-5);
+  border-radius: 25px;
+  box-shadow: 7px 7px 11px 0px rgba(0, 0, 0, 0.1),
+    -7px -7px 11px 0px rgba(77, 77, 77, 0.1);
+  &:focus {
+    outline: none;
+  }
 `
 
 const Form = () => {
@@ -27,40 +84,46 @@ const Form = () => {
     })
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
     <Container>
       <h2>Get In Touch</h2>
-      <FormContainer>
-        <form>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              value={values.name}
-              onChange={e => handleChange(e)}
-            />
-          </label>
-          <label>
-            Email
-            <input
-              type="text"
-              name="email"
-              value={values.email}
-              onChange={e => handleChange(e)}
-            />
-          </label>
-          <label>
-            Message
-            <input
-              type="text"
-              name="message"
-              value={values.message}
-              onChange={e => handleChange(e)}
-            />
-          </label>
-        </form>
-      </FormContainer>
+      <StyledForm onSubmit={e => handleSubmit(e)}>
+        <Label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            placeholder="Lionel Messi"
+            value={values.name}
+            onChange={e => handleChange(e)}
+          />
+        </Label>
+        <Label>
+          Email
+          <Input
+            type="text"
+            name="email"
+            placeholder="messi@barcelona.com"
+            value={values.email}
+            onChange={e => handleChange(e)}
+          />
+        </Label>
+        <Label>
+          Message
+          <TextArea
+            type="text"
+            name="message"
+            placeholder="Let's get in touch..."
+            value={values.message}
+            onChange={e => handleChange(e)}
+          />
+        </Label>
+        <Button type="submit">Submit</Button>
+      </StyledForm>
     </Container>
   )
 }
