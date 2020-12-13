32800bcb09eec72d67e59475ec2757b314ddebd6
Kevin Luo <kvn.luo@gmail.com>
2020-12-12 23:35:32 -0500

    about me cards

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -1,30 +1,35 @@
 import React from "react"
 
-import { ContentContainer, List } from "./Styled";
-import ListItem from "./ListItem";
+import { ContentContainer, Card, ListTitle, List } from "./Styled"
+import ListItem from "./ListItem"
 
-export default function AboutSection({about}) {
-  console.log(about);
+export default function AboutSection({ about }) {
   return (
     <ContentContainer>
-      <h3>Eduction</h3>
-      <List>
-        {about.education.map(item => (
-          <ListItem key={item.id} content={item.name} />
-        ))}
-      </List>
-      <h3>Skills/Tech</h3>
-      <List>
-        {about.stack.map(item => (
-          <ListItem key={item.id} content={item.name} />
-        ))}
-      </List>
-      <h3>Hobbies</h3>
-      <List>
-        {about.hobbies.map(item => (
-          <ListItem key={item.id} content={item.name} />
-        ))}
-      </List>
+      <Card>
+        <ListTitle>Eduction</ListTitle>
+        <List>
+          {about.education.map(item => (
+            <ListItem key={item.id} content={item.name} />
+          ))}
+        </List>
+      </Card>
+      <Card>
+        <ListTitle>Skills/Tech</ListTitle>
+        <List>
+          {about.stack.map(item => (
+            <ListItem key={item.id} content={item.name} />
+          ))}
+        </List>
+      </Card>
+      <Card>
+        <ListTitle>Hobbies</ListTitle>
+        <List>
+          {about.hobbies.map(item => (
+            <ListItem key={item.id} content={item.name} />
+          ))}
+        </List>
+      </Card>
     </ContentContainer>
   )
 }
--- src/components/AboutSection/Styled.js
+++ src/components/AboutSection/Styled.js
@@ -17,19 +17,65 @@ export const Overlay = styled.div`
 
 export const ContentContainer = styled.div`
   width: 50vw;
-  margin: 0 2rem;
+  margin: 0 auto;
   padding: 0 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+export const Card = styled.div`
+  /* border: 1px solid white; */
+  padding: 3em;
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    background: var(--clr-grey-4);
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 20%;
+    z-index: -7;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    background: white;
+    width: 0%;
+    height: 100%;
+    background: green;
+    right: 0;
+    top: 0;
+    z-index: -6;
+  }
+`
+
+export const ListTitle = styled.h3`
+
 `
 
 export const List = styled.ul`
   list-style: none;
   display: flex;
   flex-wrap: wrap;
-  margin-bottom: 2rem;
+  padding: 1.5em 0 0 3em;
+  position: relative;
+
   `
 
 export const Item = styled.li`
   flex: 1 1 auto;
   margin-right: .5rem;
-  margin-bottom: 1rem;
+  clip-path: circle(0% at 100% 0);
+  &:before {
+    content: "";
+    position: absolute;
+    height: 80%;
+    width: 4px;
+    background: darkblue;
+    left: 0;
+    top: 1.6em;
+  }
 `
+
